261d54b7a4314406e09fbcd18508ea19b575438e
MonsefRH <monsef.rh15@gmail.com>
2025-04-22 00:34:33 +0100

    filament added successfully

--- resources/views/bookings/create.blade.php
+++ resources/views/bookings/create.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-2xl font-bold mb-4">Book {{ $property->name }}</h1>
     @livewire('booking-manager', ['propertyId' => $property->id])
 @endsection
